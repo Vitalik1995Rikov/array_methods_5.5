bc3ec6af9c31452afe590d5e82de5a2d9b0ba9cf
Vitalik1995Rikov <68601312+Vitalik1995Rikov@users.noreply.github.com>
2021-04-03 23:28:36 +0300

    ex2

--- README.md
+++
@@ -1,1 +0,0 @@
-# array_methods_5.5
--- script.js
+++
@@ -1,8 +0,0 @@
-﻿let frase = "border-left-width";
-let arr = frase.split('-');
-arrNew = arr.join('');
-arrNew2 = arrNew.split('');
-arrNew2.splice(6, 1, 'L');
-arrNew2.splice(10, 1, 'W');
-let arrFinish = arrNew2.join('');
-console.log(arrFinish);
